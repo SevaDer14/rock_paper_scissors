033c5f931c2261c4105cd9d9a99d8da282ea87ae
sevader14 <vsevolod.deriushkin@gmail.com>
2021-04-28 16:28:59 +0200

    adds hand coloring feature

--- src/GameContainer.jsx
+++ src/GameContainer.jsx
@@ -4,23 +4,30 @@ import gameLogic from "../src/GameLogic.jsx";
 
 
 const GameContainer = () => {
-  const [playerHand, setPlayerHand] = useState("Your hand of choice");
-  const [computerHand, setComputerHand] = useState("PC hand will be here");
+  const [playerHand, setPlayerHand] = useState("Your hand");
+  const [computerHand, setComputerHand] = useState("PC hand");
   const [resultMessage, setResult] = useState("");
   const [playerScore, setPlayerScore] = useState(0);
   const [computerScore, setComputerScore] = useState(0);
+  const [playerHandColor, setPlayerHandColor] = useState('black');
+  const [computerHandColor, setComputerHandColor] = useState('black');
 
   const handleOutcome = (gameOutcome) => {
     if (gameOutcome.tie) {
       setResult('Tie');
+      setPlayerHandColor('black')
+      setComputerHandColor('black')
     } else if (gameOutcome.player) {
       setResult('Player Wins!');
       setPlayerScore(playerScore + 1)
+      setPlayerHandColor('green')
+      setComputerHandColor('red')
     } else if (gameOutcome.computer) {
       setResult('Computer Wins!');
       setComputerScore(computerScore + 1)
-    }
-    
+      setComputerHandColor('green')
+      setPlayerHandColor('red')
+    }    
   }
 
   const startGame = (playerHand) => {
@@ -62,11 +69,17 @@ const GameContainer = () => {
         <Grid columns="2">
           <Grid.Column>
             <Header data-cy='player-score'>Player: {playerScore}</Header>
-            <Segment data-cy="player1-hand-display">{playerHand}</Segment>
+            <Segment 
+              data-cy="player-hand-display" 
+              style={{fontSize: '28px', color: playerHandColor}}
+              >{playerHand}</Segment>
           </Grid.Column>
           <Grid.Column>
             <Header data-cy='computer-score'>Computer: {computerScore}</Header>
-            <Segment data-cy="player2-hand-display">{computerHand}</Segment>
+            <Segment 
+              data-cy="computer-hand-display" 
+              style={{fontSize: '28px', color: computerHandColor}}
+              >{computerHand}</Segment>
           </Grid.Column>
         </Grid>
       </Segment>
